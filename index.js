34f6cea63e67a15c96c6d746219ba74712f6584f
Tej Patel <tejpatel317@gmail.com>
2022-05-08 22:45:08 -0400

    commit tree 46f5cc6036362258a07c8c4f77de0248c50dd754

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+function submitData(username, userEmail){
+    let userData = {name : username, email : userEmail}
+    
+    return fetch("http://localhost:3000/users",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(userData)
+    })
+    .then((resp) => resp.json())
+    .then((data) => {
+        const element = document.createElement("p")
+        element.textContent = data.id
+        document.body.appendChild(element)
+    })
+    .catch((message) => {
+        const errorMessage = document.createElement("p")
+        errorMessage.textContent = message
+        document.body.appendChild(errorMessage)
+    })
+}
